d914fd38b426d58c4eeb5ed8e1ea9551f2421d0f
Serena Tran <serenatran@Serenas-MacBook-Air.local>
2026-06-01 14:39:39 +0700

    Fix Fierce Tooth enemy animation

--- classes/enemy.py
+++ classes/enemy.py
@@ -1,7 +1,9 @@
 import pygame
+import os
+
 
 class Enemy:
-    def __init__(self, x, y, width=40, height=40, speed=2, patrol_distance=120):
+    def __init__(self, x, y, width=120, height=120, speed=2, patrol_distance=120):
         self.rect = pygame.Rect(x, y, width, height)
         self.start_x = x
         self.speed = speed
@@ -10,15 +12,60 @@ class Enemy:
 
         self.damage = 1
         self.last_hit_time = 0
-        self.hit_cooldown = 1000  # 1000 ms = 1 second
+        self.hit_cooldown = 1000
+
+        self.width = width
+        self.height = height
+
+        self.animations = {
+            "idle": self.load_animation("01-Idle"),
+            "run": self.load_animation("02-Run"),
+        }
+
+        self.state = "run"
+        self.frame_index = 0
+        self.animation_speed = 0.15
+        self.image = self.animations[self.state][0]
+
+    def load_animation(self, folder_name):
+        frames = []
+
+        folder_path = os.path.join(
+            "asset",
+            "enemy",
+            "Fierce Tooth",
+            folder_name
+        )
+
+        for file_name in sorted(os.listdir(folder_path)):
+            if file_name.endswith(".png"):
+                image_path = os.path.join(folder_path, file_name)
+                image = pygame.image.load(image_path).convert_alpha()
+                image = pygame.transform.scale(image, (self.width, self.height))
+                frames.append(image)
+
+        return frames
 
     def move(self):
         self.rect.x += self.speed * self.direction
 
-        if self.rect.x > self.start_x + self.patrol_distance:
+        if self.rect.x >= self.start_x + self.patrol_distance:
             self.direction = -1
-        elif self.rect.x < self.start_x:
+        elif self.rect.x <= self.start_x:
             self.direction = 1
+
+    def animate(self):
+        frames = self.animations[self.state]
+
+        self.frame_index += self.animation_speed
+
+        if self.frame_index >= len(frames):
+            self.frame_index = 0
+
+        self.image = frames[int(self.frame_index)]
+
+        if self.direction == -1:
+            self.image = pygame.transform.flip(self.image, True, False)
 
     def attack_player(self, player):
         current_time = pygame.time.get_ticks()
@@ -31,7 +78,14 @@ class Enemy:
 
     def update(self, player):
         self.move()
+
+        if self.speed == 0:
+            self.state = "idle"
+        else:
+            self.state = "run"
+
+        self.animate()
         self.attack_player(player)
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.image, self.rect)
